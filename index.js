88720a26b2cb3ee4b8d094d7916b5da0f58398db
Vitaly Kunitsa <vitaly.steel@gmail.com>
2016-12-28 01:02:50 +0300

    Some node-pre-gyp settings up 2

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 'use strict';
+var binary = require('node-pre-gyp');
+var path = require('path');
+var binding_path = binary.find(path.resolve(path.join(__dirname, './package.json')));
+var binding = require(binding_path);
 
 for (let mod of ["hdf5", "h5lt", "h5tb", "h5pt", "h5im", "h5ds"]) {
   module.exports[mod] = require(`./build/Release/${mod}`);
